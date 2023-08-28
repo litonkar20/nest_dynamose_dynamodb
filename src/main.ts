d8eb6a736f3838d5b2e47afca7d1b9f947c409ca
Liton Kar1 <litokar1@in.ibm.com>
2023-08-28 14:11:53 +0530

    DinamooDB

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,6 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
